aa1145ef919987d1a162892287a6fb1a0ff3b1e1
infoscience <kariukilewis04@gmail.com>
2025-05-13 08:06:50 +0300

    Updated  sections

--- src/class2/logicaloperatorsinjava.java
+++ src/class2/logicaloperatorsinjava.java
@@ -5,7 +5,7 @@ public class logicaloperatorsinjava {
     int weight = 80;
     boolean canRide =  (age > 18 && weight > 50);
     public static void main (String[] args) {
-        logicaloperatorsinjava obj = new logicaloperatorsinjava();
+        logicaloperatorsinjava obj =  new logicaloperatorsinjava();
         System.out.println(obj.canRide);
     }
 }
--- src/class2/operatorsinjava.java
+++ src/class2/operatorsinjava.java
@@ -2,7 +2,7 @@ package class2;
 
 public class operatorsinjava {
     int yearofbirth = 1999;
-    int currentyear = 2022;
+    int currentyear = 2025;
     int age = currentyear - yearofbirth;
     public static void main (String[] args) {
         operatorsinjava obj = new operatorsinjava();
